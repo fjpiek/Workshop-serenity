d9cecdb12522ba459a74abadae000e1d53a3e859
unknown <freekjanp@FREEKJANP02.corp.infosupport.com>
2015-09-26 13:16:09 +0200

    solutions assignment 1, 2 and 3

---
+++ src/test/java/com/infosupport/serenity/Petstore.java
@@ -0,0 +1,9 @@
+package com.infosupport.serenity;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/api")
+public class Petstore {}
