580afdf92415051711b25fdbeb5ef98eb9ab368d
unknown <freekjanp@FREEKJANP02.corp.infosupport.com>
2015-09-27 13:42:14 +0200

    removed unused directory and moved the seacr_products feature file

--- src/test/java/com/infosupport/serenity/SearchProducts.java
+++ src/test/java/com/infosupport/serenity/SearchProducts.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/search/search_products.feature")
+@CucumberOptions(features="src/test/resources/features/search_products.feature")
 public class SearchProducts {}
